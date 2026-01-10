fe57338fdfee7586874a1997259a5ddd30f09c63
Hugo Duncan <hugo@hugoduncan.org>
2026-01-10 13:54:39 -0500

    refactor: hide link computation code in index notebook
    Use ^:kindly/hide-code to hide implementation details from rendered
    output while keeping the benchmark links visible. Add GitHub link.

--- notebooks/index.clj
+++ notebooks/index.clj
@@ -1,5 +1,6 @@
 (ns index
-  {:clay {:title "Clojure Language Benchmarks"}}
+  {:clay             {:title "Clojure Language Benchmarks"}
+   :kindly/hide-code true}
   (:require
    [clojure.edn :as edn]
    [clojure.java.io :as io]
@@ -10,67 +11,55 @@
 ;;
 ;; This project benchmarks Clojure implementations using criterium,
 ;; with results published as Clay notebooks.
+;;
+;; [Source on GitHub](https://github.com/hugoduncan/clj-lang-benchmarks)
 
 ;; ## Benchmarks
 
-(defn- notebook-files
-  "Returns .clj files in notebooks/ excluding index.clj."
-  []
-  (->> (io/file "notebooks")
-       (.listFiles)
-       (filter #(and (.isFile %)
-                     (str/ends-with? (.getName %) ".clj")
-                     (not= "index.clj" (.getName %))))
-       (sort-by #(.getName %))))
+^:kindly/hide-code
+(do
+  (defn- notebook-files
+    "Returns .clj files in notebooks/ excluding index.clj."
+    []
+    (->> (io/file "notebooks")
+         (.listFiles)
+         (filter #(and (.isFile %)
+                       (str/ends-with? (.getName %) ".clj")
+                       (not= "index.clj" (.getName %))))
+         (sort-by #(.getName %))))
 
-(defn- read-ns-form
-  "Reads the first form from a file, expected to be an ns form."
-  [file]
-  (with-open [rdr (java.io.PushbackReader. (io/reader file))]
-    (edn/read rdr)))
+  (defn- read-ns-form
+    "Reads the first form from a file, expected to be an ns form."
+    [file]
+    (with-open [rdr (java.io.PushbackReader. (io/reader file))]
+      (edn/read rdr)))
 
-(defn- extract-title
-  "Extracts :clay {:title ...} from ns form metadata, or derives from filename."
-  [file ns-form]
-  (let [ns-meta (when (and (sequential? ns-form)
-                           (>= (count ns-form) 2)
-                           (= 'ns (first ns-form)))
-                  (let [second-elem (nth ns-form 2 nil)]
-                    (when (map? second-elem) second-elem)))]
-    (or (get-in ns-meta [:clay :title])
-        (-> (.getName file)
-            (str/replace #"\.clj$" "")
-            (str/replace "_" " ")
-            str/capitalize))))
+  (defn- extract-title
+    "Extracts :clay {:title ...} from ns form metadata, or derives from filename."
+    [file ns-form]
+    (let [ns-meta (when (and (sequential? ns-form)
+                             (>= (count ns-form) 2)
+                             (= 'ns (first ns-form)))
+                    (let [second-elem (nth ns-form 2 nil)]
+                      (when (map? second-elem) second-elem)))]
+      (or (get-in ns-meta [:clay :title])
+          (-> (.getName file)
+              (str/replace #"\.clj$" "")
+              (str/replace "_" " ")
+              str/capitalize))))
 
-(defn- notebook-link
-  "Generates hiccup for a notebook link."
-  [file]
-  (let [ns-form (read-ns-form file)
-        title (extract-title file ns-form)
-        html-name (-> (.getName file)
-                      (str/replace #"\.clj$" ".html"))]
-    [:li [:a {:href html-name} title]]))
+  (defn- notebook-link
+    "Generates hiccup for a notebook link."
+    [file]
+    (let [ns-form   (read-ns-form file)
+          title     (extract-title file ns-form)
+          html-name (-> (.getName file)
+                        (str/replace #"\.clj$" ".html"))]
+      [:li [:a {:href html-name} title]]))
+  "")
 
+^:kindly/hide-code
 (kind/hiccup
  (into [:ul]
        (map notebook-link)
        (notebook-files)))
-
-;; ## Running Benchmarks
-;;
-;; To run benchmarks locally:
-;;
-;; ```bash
-;; clj -M:dev
-;; ```
-;;
-;; Then evaluate the benchmark namespace in your REPL.
-
-;; ## Building Notebooks
-;;
-;; ```bash
-;; clj -T:build notebooks
-;; ```
-;;
-;; Output is written to `docs/` for GitHub Pages.
